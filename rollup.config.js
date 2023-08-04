3d3b8476649dbb51a50eab20fc244b8d497294de
Pavel Ivanov <grammka@gmail.com>
2023-08-04 17:39:36 +0200

    update config

--- rollup.config.js
+++ rollup.config.js
@@ -5,34 +5,60 @@ import preserverDirectives from 'rollup-plugin-preserve-directives'
 /**
  * @type {import('rollup').RollupOptions}
  */
-export default {
-  input: [
-    'src/index.ts',
-    'src/react/apollo.tsx',
-    'src/nextjs/apollo.tsx',
-  ],
-  output: {
-    dir: 'dist',
-    entryFileNames: '[name].js',
-    format: 'es',
-    preserveModules: true,
+export default [
+  {
+    input: {
+      index: 'src/index.ts',
+      config: 'src/config.ts',
+      apollo: 'src/react/apollo.tsx',
+    },
+    output: {
+      dir: 'dist',
+      entryFileNames: (chunkInfo) => {
+        if (chunkInfo.name === 'apollo') {
+          return 'react/[name].js'
+        }
+
+        return '[name].js'
+      },
+      format: 'es',
+    },
+    external: [
+      /@apollo/,
+      /graphql/,
+      /react/,
+      /viem/,
+      /wagmi/,
+    ],
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+      }),
+    ],
   },
-  external: [
-    /@apollo/,
-    /graphql/,
-    /react/,
-    /viem/,
-    /wagmi/,
-  ],
-  plugins: [
-    typescript({
-      tsconfig: './tsconfig.json',
-    }),
-    preserverDirectives(),
-  ],
-  onwarn: (warning, warn) => {
-    if (warning.code !== 'MODULE_LEVEL_DIRECTIVE') {
-      warn(warning)
-    }
+  {
+    input: 'src/nextjs/apollo.tsx',
+    output: {
+      dir: 'dist',
+      entryFileNames: '[name].js',
+      format: 'es',
+      preserveModules: true,
+    },
+    external: [
+      /@apollo/,
+      /react/,
+      /viem/,
+    ],
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+      }),
+      preserverDirectives(),
+    ],
+    onwarn: (warning, warn) => {
+      if (warning.code !== 'MODULE_LEVEL_DIRECTIVE') {
+        warn(warning)
+      }
+    },
   },
-}
+]
