813872e9559c28be572ac4c0a93cf6d23c46080c
CleanBread <gleb4ist@gmail.com>
2023-11-03 12:01:13 +0100

    fix hydration error

--- examples/nextjs/src/components/Header.tsx
+++ examples/nextjs/src/components/Header.tsx
@@ -2,12 +2,8 @@
 import React from 'react'
 import { ConnectButton } from '@rainbow-me/rainbowkit'
 import { ActiveLink, SelectAppChain } from '@/components'
-import { useAccount } from 'wagmi'
-
 
 export function Header() {
-  const { address } = useAccount()
-
   return (
     <header className="container flex items-center py-3.5 border-b border-zinc-200">
       <div className="text-xl font-semibold">Azuro Betting</div>
@@ -20,8 +16,7 @@ export function Header() {
         >
           Events
         </ActiveLink>
-        {address && (
-          <ActiveLink
+        <ActiveLink
           className="text-zinc-500 hover:text-black transition ml-4"
           activeClassName="!text-black font-semibold !cursor-default"
           href="/bets"
@@ -29,7 +24,6 @@ export function Header() {
         >
           Bets
         </ActiveLink>
-        )}
       </div>
       <div className="ml-auto flex items-center">
         <SelectAppChain />
