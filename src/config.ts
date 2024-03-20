f269ea17a29325e7de71fa7c7dabb83fbb523dd8
CleanBread <gleb4ist@gmail.com>
2024-03-20 12:50:34 +0100

    update addresses

--- src/config.ts
+++ src/config.ts
@@ -102,7 +102,7 @@ const gnosisData: ChainData = {
     lp: '0x204e7371Ade792c5C006fb52711c50a7efC843ed',
     prematchCore: '0x7f3F3f19c4e4015fd9Db2f22e653c766154091EF',
     prematchComboCore: '0xDbC3BE2DDB53e1a288F7b7a4d020F8056D3b0F7C',
-    proxyFront: '0xaAAc47Bb9B16bcF89FdB54B1326327a223458d6d',
+    proxyFront: '0x3A1c6640daeAc3513726F06A9f03911CC1080251',
   }),
   betToken: {
     address: '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d',
@@ -117,7 +117,7 @@ const polygonData: ChainData = {
     lp: '0x7043E4e1c4045424858ECBCED80989FeAfC11B36',
     prematchCore: '0xA40F8D69D412b79b49EAbdD5cf1b5706395bfCf7',
     prematchComboCore: '0x92a4e8Bc6B92a2e1ced411f41013B5FE6BE07613',
-    proxyFront: '0x200BD65A3189930634af857C72281abE63C3da5e',
+    proxyFront: '0x0DEE52b98ba8326DaD4C346a4F806Fd871360a00',
   }),
   betToken: {
     address: '0xc2132d05d31c914a87c6611c10748aeb04b58e8f',
@@ -132,7 +132,7 @@ const polygonMumbaiData: ChainData = {
     lp: '0xA39e988b357122D9ba75C0038E625a5ab9363F54',
     prematchCore: '0x33742d3D8bE5A7F47D2eC1fA39fDa9FCB5288727',
     prematchComboCore: '0xb7f1B3dA0eECf0a084cdd16Df917216BB72b42f5',
-    proxyFront: '0x545715348d9ba42Bf8Cc907Be323283EF616E807',
+    proxyFront: '0x0a727CE540258f9736eAfcD411e57Caf5a3B3b6C',
     liveRelayer: '0x2d14b12e08ED8599E020fdBC867B604Ad58F192c',
   }),
   betToken: {
