ccf05b4201bb7507c9bde2e652251cc53661185a
CleanBread <gleb4ist@gmail.com>
2024-04-15 10:14:59 +0200

    filter supported chains by appChain

--- src/hooks/useDeBridgeSupportedChains.ts
+++ src/hooks/useDeBridgeSupportedChains.ts
@@ -1,6 +1,8 @@
 import { useQuery } from '@tanstack/react-query'
+import { useMemo } from 'react'
 
 import { deBridgeUrl } from 'src/config'
+import { useChain } from 'src/contexts/chain'
 
 
 type SupportedChainsResponse = {
@@ -15,16 +17,13 @@ type Props = {
 }
 
 export const useDeBridgeSupportedChains = ({ enabled }: Props = { enabled: true }) => {
+  const { appChain } = useChain()
+
   const queryFn = async () => {
     const response = await fetch(`${deBridgeUrl}/supported-chains-info`)
     const { chains }: SupportedChainsResponse = await response.json()
 
-    const chainIds = chains.map(({ chainId }) => chainId)
-
-    return {
-      chains,
-      chainIds,
-    }
+    return chains
   }
 
   const { isFetching, data, refetch } = useQuery({
@@ -34,9 +33,23 @@ export const useDeBridgeSupportedChains = ({ enabled }: Props = { enabled: true 
     refetchOnWindowFocus: false,
   })
 
+  const { supportedChains, supportedChainIds } = useMemo(() => {
+    if (!data?.length) {
+      return {}
+    }
+
+    const filteredChains = data?.filter(chain => chain.chainId !== appChain.id)
+    const supportedChainIds = filteredChains.map(({ chainId }) => chainId)
+
+    return {
+      supportedChains: filteredChains,
+      supportedChainIds,
+    }
+  }, [ appChain.id, data ])
+
   return {
-    supportedChains: data?.chains,
-    supportedChainIds: data?.chainIds,
+    supportedChains,
+    supportedChainIds,
     refetch,
     loading: isFetching,
   }
