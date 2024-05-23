3e64d170130a19857c8fcf7e21618b7f3d71360d
CleanBread <gleb4ist@gmail.com>
2024-05-23 11:05:53 +0200

    fix calcMinOdds

--- src/utils/calcMindOdds.ts
+++ src/utils/calcMindOdds.ts
@@ -10,5 +10,5 @@ export const calcMindOdds = (props: CalcMindOddsProps) => {
   const totalOdds = typeof props.odds === 'number' ? props.odds : props.odds.reduce((acc, odds) => acc * +odds, 1)
   const minOdds = 1 + (totalOdds - 1) * (100 - props.slippage) / 100
 
-  return parseFloat(String(minOdds)).toFixed(ODDS_DECIMALS)
+  return minOdds.toFixed(ODDS_DECIMALS)
 }
